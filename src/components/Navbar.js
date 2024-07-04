35b382e2e09f55b10a9c897e1b2755d98c6d9ebf
Deceit77 <ishanlamsal3434@gmail.com>
2024-07-04 12:13:52 +0530

    After logi

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logom from './logom.png'; // Adjust the path as needed
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,7 +12,10 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      <div className="navbar-brand">Ishan Lamsal</div>
+      <div className="navbar-brand">
+        <img src={logom} alt="Logo" className="navbar-logo" />
+        Ishan Lamsal
+      </div>
       <button className={`navbar-toggler ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
         <span className="navbar-toggler-icon">&#9776;</span>
       </button>
